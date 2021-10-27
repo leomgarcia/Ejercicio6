2bf91bdde8915ea4cc91d0d5aee9e88244f56370
leon garcia <leongarcia787@gmail.com>
2021-10-27 11:46:43 +0200

    Ejercicio6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import statistics as stats
 
 Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
 print(stats.mean(Listado))
-edades = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
+Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
 print(stats.median(Listado))
-edades = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
+Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
 print(stats.pvariance(Listado))
