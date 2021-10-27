b217371925a77faa7f2a699da3f0e50fdda1dc07
leon garcia <46096370+leomgarcia@users.noreply.github.com>
2021-10-27 11:47:34 +0200

    Delete EJ6 Calse.py

--- EJ6 Calse.py
+++
@@ -1,7 +0,0 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-word = input("Palabra:")
-v_freq = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0 }
-for letter in word:
-    if letter in vowels:
-        v_freq[letter] += 1
-print(f"Frecuencia de las Vocales en {word}:\n{v_freq}", word, v_freq)
