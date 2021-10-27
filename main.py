b0823e6bf12431e8d44da9309e3e8896e449c8c5
leon garcia <46096370+leomgarcia@users.noreply.github.com>
2021-10-27 22:48:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import statistics as stats
+import statistics
 
-Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
-print(stats.mean(Listado))
-Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
-print(stats.median(Listado))
-Listado = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
-print(stats.pvariance(Listado))
+n1 = int(input("Ingrese primer dato númerico :"))
+n2 = int(input("Ingrese el segundo dato númerico :"))
+n3 = int(input("Ingrese el tercer dato númerico :"))
+list = [n1, n2, n3]
+
+mean = statistics.mean(list)
+print("el valor medio es :" + str(mean))
+
+median = statistics.median(list)
+print("El valor de la mediana es :" + str(median))
+
+variance = statistics.variance(list)
+print("El valor de la variante es :" + str(variance))
